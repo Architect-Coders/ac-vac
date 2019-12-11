746c0552e6138a564ef7b84439357d2aa23d8310
David RS <dondavidrs+programming@gmail.com>
2019-12-11 20:11:44 +0100

    Add ToolbarAppBar and BottomAppBar

---
+++ app/src/main/java/com/example/vacunas/ui/user/UserListFragment.kt
@@ -0,0 +1,18 @@
+package com.example.vacunas.ui.blank
+
+import com.example.vacunas.R
+import com.example.vacunas.base.ui.BaseFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class BlankFragment : BaseFragment<BlankViewModel>() {
+
+    override val viewModel: BlankViewModel by viewModel()
+
+    //region Override BaseFragment methods
+    override fun getLayoutId(): Int = R.layout.fragment_blank
+
+    override fun setObservers() {}
+
+    override fun setupView() {}
+    //endregion
+}
